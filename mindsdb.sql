03ef5819453869e05e99aa76892d94e4017c0cb2
Ayush <ayush.gupta2020c@vitstudent.ac.in>
2024-01-03 12:37:59 +0530

    chore: adding the mindsdb sql queries that were executed in the mindsdb editor

---
+++ mindsdb.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE btc_datasource
+WITH
+  engine = 'mariadb',
+  parameters = {
+    "host": "",       -- EC2 public IP here
+    "port": 3306,
+    "database": "btc_price",
+    "user": "mindsdb",
+    "password": ""    -- mindsdb user password here
+  };
+
+SELECT * FROM btc_datasource.btc_price_data;
+
+SHOW ML_ENGINES;
+
+DROP PREDICTOR mindsdb.btc_price_predictor;
+
+CREATE MODEL mindsdb.btc_price_predictor
+FROM btc_datasource (
+  SELECT * FROM btc_price_data
+  )
+PREDICT Open
+ORDER BY Datetime
+WINDOW 2160
+HORIZON 72
+USING ENGINE = 'statsforecast';
+
+DESCRIBE btc_price_predictor;
+
+SELECT m.Datetime as Datetime, m.Open as forecasted_open
+FROM mindsdb.btc_price_predictor as m
+JOIN btc_datasource.btc_price_data as t;
+
